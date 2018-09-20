dbc21196b815b64b3c0b029a95268bf0f7baa6dc
Noah Rubin <nrubin29@gmail.com>
2018-09-19 22:17:25 -0400

    Added form.select to further hide implementation details.

--- lib.py
+++ lib.py
@@ -7,20 +7,6 @@ from selenium import webdriver
 from selenium.webdriver.support.select import Select
 
 
-def _get_element(driver, name, id, selector):
-    if (not name and not id and not selector) or any([a and b for a, b in itertools.permutations([name, id, selector], 2)]):
-        raise Exception('Must specify name xor id xor selector')
-
-    if name:
-        return driver.find_element_by_name(name)
-
-    elif id:
-        return driver.find_element_by_id(id)
-
-    else:
-        return driver.find_element_by_css_selector(selector)
-
-
 class Form:
     def __init__(self, url, headless=False):
         self.url = url
@@ -31,9 +17,44 @@ class Form:
         self.driver.implicitly_wait(3)
         self.driver.get(self.url)
 
+    def select(self, name=None, id=None, selector=None, nth_child=None, **kwargs):
+        if (not name and not id and not selector) or any(
+                [a and b for a, b in itertools.permutations([name, id, selector], 2)]):
+            raise Exception('Must specify name xor id xor selector')
+
+        if name:
+            elems = self.driver.find_elements_by_name(name)
+
+        elif id:
+            elems = self.driver.find_elements_by_id(id)
+
+        else:
+            elems = self.driver.find_elements_by_css_selector(selector)
+
+        elem = elems[0 if nth_child is None else nth_child]
+
+        if elem.tag_name == 'input':
+            if elem.get_attribute('type') == 'submit' or elem.get_attribute('type') == 'button':
+                return ButtonFormElement(elem)
+
+            else:
+                return TextFormElement(elem **kwargs)
+
+        elif elem.tag_name == 'button' or elem.tag_name == 'a':
+            return ButtonFormElement(elem)
+
+        elif elem.tag_name == 'textarea':
+            return TextFormElement(elem, **kwargs)
+
+        elif elem.tag_name == 'select':
+            return SelectFormElement(elem, **kwargs)
+
+        else:
+            return FormElement(elem)
+
     def fill(self, elements):
         for element in elements:
-            element._run(self.driver)
+            element.run(self.driver)
 
     def fill_from_row(self, row, elements):
         elems_to_fill = {element.col: element for element in elements if isinstance(element, InputFormElement)}
@@ -43,9 +64,6 @@ class Form:
 
         self.fill(elements)
 
-    def get_text(self, name=None, id=None, selector=None):
-        return _get_element(self.driver, name, id, selector).text
-
     @contextmanager
     def frame(self, id):
         self.driver.switch_to.frame(self.driver.find_element_by_id(id))
@@ -54,22 +72,21 @@ class Form:
 
 
 class FormElement(ABC):
-    def __init__(self, name=None, id=None, selector=None):
-        self.name = name
-        self.id = id
-        self.selector = selector
+    def __init__(self, element):
+        self.element = element
 
-    def _run(self, driver):
-        self.run(_get_element(driver, self.name, self.id, self.selector))
+    @property
+    def text(self):
+        return self.element.text
 
     @abstractmethod
-    def run(self, element):
-        raise Exception('Not implemented')
+    def run(self):
+        pass
 
 
 class InputFormElement(FormElement, ABC):
-    def __init__(self, name=None, id=None, selector=None, val=None, col=None):
-        super().__init__(name, id, selector)
+    def __init__(self, element, val=None, col=None):
+        super().__init__(element)
         self.val = val
         self.col = col
 
@@ -77,47 +94,44 @@ class InputFormElement(FormElement, ABC):
             raise Exception('Must specify val or col')
 
     @abstractmethod
-    def run(self, element):
-        raise Exception('Not implemented')
+    def run(self):
+        pass
 
 
 class TextFormElement(InputFormElement):
-    def __init__(self, name=None, id=None, selector=None, val=None, col=None):
-        super().__init__(name, id, selector, val, col)
+    def __init__(self, element, val=None, col=None):
+        super().__init__(element, val, col)
 
-    def run(self, element):
+    def run(self):
         if not self.val:
             raise Exception('Missing val')
 
-        element.send_keys(self.val)
+        self.element.send_keys(self.val)
 
 
 class SelectFormElement(InputFormElement):
-    def __init__(self, name=None, id=None, selector=None, val=None, col=None):
-        super().__init__(name, id, selector, val, col)
+    def __init__(self, element, val=None, col=None):
+        super().__init__(element, val, col)
 
-    def run(self, element):
+    def run(self):
         if not self.val:
             raise Exception('Missing val')
 
-        Select(element).select_by_visible_text(self.val)
+        Select(self.element).select_by_visible_text(self.val)
 
 
 class ButtonFormElement(FormElement):
-    def __init__(self, name=None, id=None, selector=None):
-        super().__init__(name, id, selector)
+    def __init__(self, element):
+        super().__init__(element)
 
-    def run(self, element):
-        element.click()
+    def run(self):
+        self.element.click()
 
 
 class SleepElement(FormElement):
     def __init__(self, seconds):
-        super().__init__(name='__sleep__')
+        super().__init__(None)
         self.seconds = seconds
 
-    def _run(self, driver):
+    def run(self):
         time.sleep(self.seconds)
-
-    def run(self, element):
-        pass
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from file_lib import Spreadsheet
-from lib import TextFormElement, ButtonFormElement, SleepElement, Form
+from lib import SleepElement, Form
 
 # We create a reference to our spreadsheet so that we can fill data from it and update it.
 spreadsheet = Spreadsheet('sample.csv')
@@ -10,17 +10,17 @@ form = Form('https://www.e-oscar-web.net/EntryController?trigger=Login')
 
 # First we need to log in.
 form.fill([
-    TextFormElement(id='companyId', val='RegID'),
-    TextFormElement(id='userId', val='UserID'),
-    TextFormElement(id='password', val='password'),
-    ButtonFormElement(id='securityMsgAck1'),
+    form.select(id='companyId', val='RegID'),
+    form.select(id='userId', val='UserID'),
+    form.select(id='password', val='password'),
+    form.select(id='securityMsgAck1'),
     SleepElement(3),  # For demonstration purposes only. Fills in info and then waits 3 seconds.
-    ButtonFormElement(selector='.loginBtn')
+    form.select(selector='.loginBtn')
 ])
 
 with form.frame('topFrame'):
     form.fill([
-        ButtonFormElement(id='AUDButton')
+        form.select(id='AUDButton')
     ])
 
 # Clicking the .loginBtn will cause the form to submit and we'll be navigated to a new page.
@@ -28,17 +28,17 @@ with form.frame('topFrame'):
 for row in spreadsheet.rows:
     #  We can update the spreadsheet with data from the form.
     #  Here, we are updating the AUD column of each row to be the text of the #aud element.
-    row.set('AUD', form.get_text(id='aud'))
+    row.set('AUD', form.select(id='aud'))
 
     # Because we are using fill_from_row, the elements will automatically get their values from the spreadsheet
     # for the current row and at the column specified by the `col` field.
     form.fill_from_row(row, [
-        TextFormElement(id='companyId', col='Company ID'),
-        TextFormElement(id='userId', col='User ID'),
-        TextFormElement(id='password', col='Password'),
-        ButtonFormElement(id='securityMsgAck1'),
+        form.select(id='companyId', col='Company ID'),
+        form.select(id='userId', col='User ID'),
+        form.select(id='password', col='Password'),
+        form.select(id='securityMsgAck1'),
         SleepElement(3),
-        ButtonFormElement(selector='.loginBtn')
+        form.select(selector='.loginBtn')
     ])
 
 # After updating the spreadsheet, make sure to save it back to the file.
